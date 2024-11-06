583008eb323ae9078b0cf57c08d0fa4360bd396a
saphess <saphess@mail.ru>
2024-11-06 13:07:56 +0300

    new validation test

--- src/test/java/ru/netology/selenium/CallbackTest.java
+++ src/test/java/ru/netology/selenium/CallbackTest.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CallbackTest {
     private WebDriver driver;
@@ -57,17 +58,19 @@ public class CallbackTest {
     public void shouldBeValidationName() {
         WebElement form = driver.findElement(By.cssSelector("form"));
         form.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ivan Ivanovich");
+        form.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+71231231212");
+        form.findElement(By.cssSelector("[data-test-id='agreement']")).click();
 
         form.findElement(By.cssSelector("button")).click();
 
-        String actualText = form.findElement(By.cssSelector(".input_invalid .input__sub")).getText().trim();
+        String actualText = form.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
         String expectText = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
 
-        String actualColor = form.findElement(By.cssSelector(".input_invalid .input__sub")).getCssValue("color");
-        String expectColor = "rgba(255, 92, 92, 1)";
+        Boolean actualDisplayed= form.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub"))
+                .isDisplayed();
 
         assertEquals(expectText, actualText);
-        assertEquals(expectColor, actualColor);
+        assertTrue(actualDisplayed);
     }
 
     @Test
@@ -75,17 +78,18 @@ public class CallbackTest {
         WebElement form = driver.findElement(By.cssSelector("form"));
         form.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Иванович");
         form.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("71231231212");
+        form.findElement(By.cssSelector("[data-test-id='agreement']")).click();
 
         form.findElement(By.cssSelector("button")).click();
 
-        String actualText = form.findElement(By.cssSelector(".input_invalid .input__sub")).getText().trim();
+        String actualText = form.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText().trim();
         String expectText = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
 
-        String actualColor = form.findElement(By.cssSelector(".input_invalid .input__sub")).getCssValue("color");
-        String expectColor = "rgba(255, 92, 92, 1)";
+        Boolean actualDisplayed = form.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub"))
+                .isDisplayed();
 
         assertEquals(actualText, expectText);
-        assertEquals(expectColor, actualColor);
+        assertTrue(actualDisplayed);
     }
 
     @Test
@@ -96,9 +100,58 @@ public class CallbackTest {
 
         form.findElement(By.cssSelector("button")).click();
 
-        String actualColor = form.findElement(By.cssSelector(".input_invalid .checkbox__text")).getCssValue("color");
-        String expectColor = "rgba(255, 92, 92, 1)";
+        Boolean actualDisplayed = form.findElement(By.cssSelector(".input_invalid .checkbox__text")).isDisplayed();
 
-        assertEquals(expectColor, actualColor);
+        assertTrue(actualDisplayed);
+    }
+
+    @Test
+    public void shouldBeValidationIfAllEmpty(){
+        WebElement form = driver.findElement(By.cssSelector("form"));
+
+        form.findElement(By.cssSelector("button")).click();
+
+        String actualText = form.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
+        String expectText = "Поле обязательно для заполнения";
+
+        Boolean actualDisplayed= form.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub"))
+                .isDisplayed();
+
+        assertEquals(expectText, actualText);
+        assertTrue(actualDisplayed);
+
+        assertEquals(expectText, actualText);
+    }
+
+    @Test
+    public void shouldBeValidationIfTelAndAgreementEmpty() {
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        form.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Иванович");
+
+        form.findElement(By.cssSelector("button")).click();
+
+        String actualText = form.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText().trim();
+        String expectText = "Поле обязательно для заполнения";
+
+        Boolean actualDisplayed= form.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub"))
+                .isDisplayed();
+
+        assertEquals(expectText, actualText);
+        assertTrue(actualDisplayed);
+
+        assertEquals(expectText, actualText);
+    }
+
+    @Test
+    public void shouldBeValidationIfAgreementEmpty() {
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        form.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Иванович");
+        form.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+71231231212");
+
+        form.findElement(By.cssSelector("button")).click();
+
+        Boolean actualDisplayed = form.findElement(By.cssSelector(".input_invalid .checkbox__text")).isDisplayed();
+
+        assertTrue(actualDisplayed);
     }
 }
